e8400c05a650e5ffe4e2947d92c8ec0e19c011bd
jviaal <gegabalen@gmail.com>
2020-06-17 20:58:37 +0800

    Uploading Files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data.json";
+// COMPONENTS
+import Header from "./components/Header";
+import JobListContainer from "./components/JobListContainer";
+// STYLES
+import { GlobalStyle } from "./styles/GlobalStyles";
+import "./styles/global.css";
 
 function App() {
+  // STATES
+  const [companyDetails, setCompanyDetails] = React.useState();
+  const [filterBy, setFilterBy] = React.useState([]);
+  // USE EFFECT
+  React.useEffect(() => {
+    const getData = async () => {
+      setCompanyDetails(data);
+      if (filterBy.length === 0) {
+      } else {
+        const temp = [];
+        data.forEach((x) => {
+          let flag = true;
+          filterBy.data.forEach((y) => {
+            if (
+              Object.values(x).includes(y) ||
+              Object.values(x.languages).includes(y) ||
+              Object.values(x.tools).includes(y)
+            ) {
+            } else {
+              return (flag = false);
+            }
+          });
+          if (flag === true) {
+            temp.push(x);
+          }
+        });
+        setCompanyDetails(temp);
+      }
+    };
+    getData();
+  }, [filterBy]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header filterBy={filterBy} setFilterBy={setFilterBy} />
+      {companyDetails && (
+        <JobListContainer
+          companyDetails={companyDetails}
+          filterBy={filterBy}
+          setFilterBy={setFilterBy}
+        />
+      )}
+      <GlobalStyle />
+    </>
   );
 }
 
